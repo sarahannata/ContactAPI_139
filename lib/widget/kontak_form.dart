87a7901919d72746fa54260a9c8f658c3c13c125
Sarah Hanna Taradipa <sarahhannata@gmail.com>
2024-04-03 20:51:51 +0700

    add on pressed to ElevatedButton Simpan

--- lib/widget/kontak_form.dart
+++ lib/widget/kontak_form.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+import 'package:data_kontak/controller/kontak_controller.dart';
+import 'package:data_kontak/model/kontak.dart';
+import 'package:data_kontak/screen/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -83,7 +86,28 @@ class _FormKontakState extends State<FormKontak> {
             Container(
               margin: const EdgeInsets.all(10),
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    var result = await KontakController().addPerson(
+                        Kontak(
+                            nama: _namaController.text,
+                            email: _emailController.text,
+                            alamat: _alamatController.text,
+                            notelp: _notelpController.text,
+                            foto: _image!.path),
+                        _image);
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                      result['message'],
+                    )));
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomeView()),
+                      (route) => false,
+                    );
+                  }
+                },
                 child: const Text("Simpan"),
               ),
             ),
