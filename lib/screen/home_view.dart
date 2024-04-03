ccc59ceb87799c70cb09e4119d3cd02e4fc21b0a
Sarah Hanna Taradipa <sarahhannata@gmail.com>
2024-04-03 20:37:24 +0700

    add controller to HomeView

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:data_kontak/controller/kontak_controller.dart';
 import 'package:data_kontak/model/kontak.dart';
 import 'package:data_kontak/widget/kontak_form.dart';
 import 'package:flutter/material.dart';
@@ -12,42 +13,51 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final KontakController _controller = KontakController();
+  @override
+  void initState() {
+    super.initState();
+    _controller.getPeople();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Daftar Orang"),
+        title: const Text("Daftar Orang"),
       ),
-      body: FutureBuilder<List<Kontak>>(builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text("Error: ${snapshot.error}"),
-          );
-        } else {
-          return ListView.builder(
-              itemCount: snapshot.data?.length ?? 0,
-              itemBuilder: (context, index) {
-                Kontak person = snapshot.data![index];
-                return ListTile(
-                  title: Text(person.nama),
-                  subtitle: Text(person.email),
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(person.foto),
-                  ),
-                );
-              });
-        }
-      }),
+      body: FutureBuilder<List<Kontak>>(
+          future: _controller.getPeople(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Error: ${snapshot.error}"),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    Kontak person = snapshot.data![index];
+                    return ListTile(
+                      title: Text(person.nama),
+                      subtitle: Text(person.email),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(person.foto),
+                      ),
+                    );
+                  });
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => FormKontak()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const FormKontak()));
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
