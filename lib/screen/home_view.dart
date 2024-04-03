a03d8b43d9b8c6bb95d536e92a0cec8d138de812
Sarah Hanna Taradipa <sarahhannata@gmail.com>
2024-04-03 20:27:11 +0700

    add Scaffold to HomeView

---
+++ lib/screen/home_view.dart
@@ -0,0 +1,46 @@
+import 'package:data_kontak/model/kontak.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Daftar Orang"),
+      ),
+      body: FutureBuilder<List<Kontak>>(builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text("Error: ${snapshot.error}"),
+          );
+        } else {
+          return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Kontak person = snapshot.data![index];
+                return ListTile(
+                  title: Text(person.nama),
+                  subtitle: Text(person.email),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(person.foto),
+                  ),
+                );
+              });
+        }
+      }),
+    );
+  }
+}
